c837251958e63ce38ec0ff52690ffb315edf9f6c
atharva120 <atharvarajkondawar21@gmail.com>
2024-10-25 16:01:16 +0530

    som

--- backend/routes/foodRoutes.js
+++ backend/routes/foodRoutes.js
@@ -7,14 +7,15 @@ const foodRoutes = express.Router();
 
 //image storage engine
 
-const storage= multer.diskStorage({
-    destination:"upload",
-    filename:(req,file,cb)=>{
-        return cb(null,`${Date.now()}${file.originalname}`)
-    }
-})
-
-const upload= multer({storage:storage})
+// const storage= multer.diskStorage({
+//     destination:"upload",
+//     filename:(req,file,cb)=>{
+//         return cb(null,`${Date.now()}${file.originalname}`)
+//     }
+// })
+const storage = multer.memoryStorage(); // Changed to memoryStorage
+const upload = multer({ storage });
+// const upload= multer({storage:storage})
 // upload.single("image"),
 //ednpoints
 foodRoutes.post("/add",upload.array("images[]",10),addFood);
