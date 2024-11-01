1892a825fd8d6306f87ab853dc39e934a90e1c1b
Atharva Rajkondawar <75412311+atharva120@users.noreply.github.com>
2024-11-02 01:00:41 +0530

    modal update

--- backend/model/foodModel.js
+++ backend/model/foodModel.js
@@ -5,7 +5,7 @@ const foodSchema = new mongoose.Schema(
         name:{type:String,required:true},
         description:{type:String,required:true},
         price:{type:String,required:true},
-        image:{type:[String],required:true},
+        image:[{type:String,required:true}],
         category:{type:String,required:true}
     }
 )
