1799fdb89170ab8e8e44d39c07554e139a5cc800
Atharva Rajkondawar <75412311+atharva120@users.noreply.github.com>
2024-11-01 09:58:23 +0530

    Update foodRoutes.js

--- backend/routes/foodRoutes.js
+++ backend/routes/foodRoutes.js
@@ -40,6 +40,9 @@ const __filename = fileURLToPath(import.meta.url);
 const __dirname = dirname(__filename);
 const uploadDir = path.join(__dirname, '../upload'); // Use __dirname for an absolute path
 
+if (!fs.existsSync(uploadDir)) {
+    fs.mkdirSync(uploadDir, { recursive: true });
+}
 
 // Set up storage engine
 const storage = multer.diskStorage({
